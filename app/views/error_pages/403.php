84254e158a24caf50e6289157295023132b85593
Jhennyfer08 <jhennyferkamilli5@gmail.com>
2026-03-31 14:31:33 -0300

    páginas de erro 403 e 404

---
+++ app/views/error_pages/403.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/assets/css/erro.css">
+    <title>Acesso Negado</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        <div class="background-403">
+        </div>
+    </main>
+
+</body>
+
+</html>
